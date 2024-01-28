d0f4db9cb54fe638d75ca0fe3ba111d4c16ec185
KKhimmoon <khin292546@gmail.com>
2024-01-29 01:29:55 +0700

    edit loginForm

--- apps/store-web/src/app/signin/page.tsx
+++ apps/store-web/src/app/signin/page.tsx
@@ -1,44 +1,13 @@
 'use client'
 
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
+import React from 'react'
 
-import { Button } from './components/button'
-import { Input } from './components/input'
+import SigninForm from './components/SigninForm'
 
 export default function SignInPage() {
   return (
     <main className="flex bg-[#030711] min-h-screen flex-col items-center justify-between p-24">
-      {/* <Form>
-        <form className="space-y-8">
-          <FormField
-            name="username"
-            // render={({ field }) => (
-            //   <FormItem>
-            //     <FormLabel>Username</FormLabel>
-            //     <FormControl>
-            //       <Input placeholder="shadcn" {...field} />
-            //     </FormControl>
-            //     <FormDescription>This is your public display name.</FormDescription>
-            //     <FormMessage />
-            //   </FormItem>
-            // )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form> */}
-      <div className="flex-col items-center justify-center">
-        <Input type="email" placeholder="Email or Phone number"></Input>
-        <Input type="password" placeholder="Password"></Input>
-        <Button>Sign In</Button>
-      </div>
+      <SigninForm />
     </main>
   )
 }
