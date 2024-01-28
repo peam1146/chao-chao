e88a05f03169f5c133a95b2dcae468292b658596
Peam <peammy2010@hotmail.com>
2024-01-28 19:38:51 +0700

    feat: mode toggle

--- apps/store-web/src/app/page.tsx
+++ apps/store-web/src/app/page.tsx
@@ -3,6 +3,7 @@ import Typography from '@/components/ui/typography'
 import Image from 'next/image'
 
 import { resolve } from '../../gqty'
+import { ModeToggle } from './components/theme-toggle'
 
 export default async function Home() {
   const data = await resolve(({ query }) => {
@@ -10,12 +11,13 @@ export default async function Home() {
   })
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row w-full items-center">
+      <div className="flex flex-row w-full items-center space-x-4">
         <Typography variant="h1" fontWeight="bold" className="flex flex-row gap-4">
           Welcome to
           <Image src={logo} width={100} height={100} alt="logo" />
           {data.email && <span>{data.email}</span>}
         </Typography>
+        <ModeToggle />
       </div>
     </main>
   )
