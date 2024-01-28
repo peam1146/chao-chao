72772b88c08d0f282147695fd4ad0483d423ce62
aIbinosaurus <thita@kkumail.com>
2024-01-28 16:51:28 +0700

    feat: add input button for sign in

--- apps/store-web/src/app/signin/page.tsx
+++ apps/store-web/src/app/signin/page.tsx
@@ -1,20 +1,43 @@
 'use client'
 
-import React from 'react'
-import React from 'react'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 
-import Image from 'next/image'
-
-import SigninForm from './components/SigninForm'
+import { Button } from './components/button'
+import { Input } from './components/input'
 
 export default function SignInPage() {
   return (
-    <main className="flex w-full bg-background min-h-screen lg:flex-row flex-col items-center justify-between p-24">
-      <div className="flex lg:w-1/2 min-w-[238px] min-h-[295px] h-full justify-center">
-        <Image src="/login.svg" alt="Sign in picture" width={497} height={602} />
-      </div>
-      <div className="flex min-w-216 lg:w-1/2 h-full pr-10 md:pr-0">
-        <SigninForm />
+    <main className="flex bg-[#030711] min-h-screen flex-col items-center justify-between p-24">
+      {/* <Form>
+        <form className="space-y-8">
+          <FormField
+            name="username"
+            // render={({ field }) => (
+            //   <FormItem>
+            //     <FormLabel>Username</FormLabel>
+            //     <FormControl>
+            //       <Input placeholder="shadcn" {...field} />
+            //     </FormControl>
+            //     <FormDescription>This is your public display name.</FormDescription>
+            //     <FormMessage />
+            //   </FormItem>
+            // )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form> */}
+      <div className="flex-col items-center justify-center">
+        <Input type="email" placeholder="Email or Phone number"></Input>
+        <Input type="password" placeholder="Password"></Input>
+        <Button>Sign In</Button>
       </div>
     </main>
   )
