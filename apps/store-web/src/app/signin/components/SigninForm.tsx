0fd2033cb2cc1b0fae5ed6f7a97f36698245a0eb
Thita <thita@kkumail.com>
2024-01-29 14:46:51 +0700

    feat: edit SigninForm component and add responsive

--- apps/store-web/src/app/signin/components/SigninForm.tsx
+++ apps/store-web/src/app/signin/components/SigninForm.tsx
@@ -7,24 +7,33 @@ import { Input } from './input'
 
 export default function SigninForm() {
   return (
-    <div className="w-[449px] h-[339px]">
+    <div className="w-full h-[339px]">
       <form className="w-full h-full">
-        <div className="w-full h-full flex flex-col justify-between">
-          <div className="w-full text-h2 font-bold text-center">Sign in</div>
-          <div className="w-full h-1/3 flex flex-col items-center justify-between">
-            <div className="w-full relative">
-              <UserCircle className="absolute ml-3 mt-3" size={20} />
-              <Input type="email" placeholder="Email or Phone number"></Input>
-            </div>
-            <div className="w-full">
-              <Input type="password" placeholder="Password"></Input>
-              <div className="text-h6 font-regular">Forget Password?</div>
+        <div className="w-full max-w-[449px] h-full flex flex-col lg:justify-center lg:gap-y-12 md:justify-between">
+          <div className="w-full flex flex-col items-center lg:justify-center lg:gap-y-12 md:gap-y-4">
+            <div className="w-full text-h2 font-bold lg:text-center text-white">Sign in</div>
+            <div className="w-full flex flex-col items-center lg:justify-center lg:gap-y-5 md:gap-y-4">
+              <div className="w-full relative">
+                <div className="absolute h-full flex items-center justify-center ml-2">
+                  <UserCircle size={20} color="#7F8EA3" />
+                </div>
+                <Input type="email" placeholder="Email or Phone number"></Input>
+              </div>
+              <div className="w-full">
+                <div className="w-full relative pb-1">
+                  <div className="absolute h-full flex items-center justify-center ml-2">
+                    <Key size={20} color="#7F8EA3" />
+                  </div>
+                  <Input type="password" placeholder="Password"></Input>
+                </div>
+                <div className="text-h6 font-regular text-white text-start">Forget Password?</div>
+              </div>
             </div>
           </div>
-          <div className="w-full flex flex-col justify-center items-center">
-            <Button>Sign In</Button>
-            <div>
-              Don't have an account? <span>Sign up</span>
+          <div className="w-full flex flex-col justify-center items-center text-white font-regular">
+            <Button className="bg-[#FF5E3A] text-h5">Sign In</Button>
+            <div className="text-h6">
+              Don't have an account? <span className="text-[#FF5E3A]">Sign up</span>
             </div>
           </div>
         </div>
