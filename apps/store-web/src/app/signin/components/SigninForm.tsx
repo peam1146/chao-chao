681174de59928651b73468f3b92fd4f56d228bdd
KKhimmoon <khin292546@gmail.com>
2024-01-31 00:21:36 +0700

    edit style in loginFrom

--- apps/store-web/src/app/signin/components/SigninForm.tsx
+++ apps/store-web/src/app/signin/components/SigninForm.tsx
@@ -1,7 +1,13 @@
 'use client'
 
-import { Key, UserCircle } from '@phosphor-icons/react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 
+import Typography from '@/components/ui/typography'
+import { Key, UserCircle } from '@phosphor-icons/react'
+import Link from 'next/link'
+
+import { useMutation } from '../../../../gqty'
 import { Button } from './button'
 import { Input } from './input'
 
@@ -9,32 +15,39 @@ export default function SigninForm() {
   return (
     <div className="w-full h-[339px]">
       <form className="w-full h-full">
-        <div className="w-full max-w-[449px] h-full flex flex-col lg:justify-center lg:gap-y-12 md:justify-between">
-          <div className="w-full flex flex-col items-center lg:justify-center lg:gap-y-12 md:gap-y-4">
-            <div className="w-full text-h2 font-bold lg:text-center text-white">Sign in</div>
-            <div className="w-full flex flex-col items-center lg:justify-center lg:gap-y-5 md:gap-y-4">
+        <div className="w-full max-w-[449px] h-full flex flex-col lg:justify-center gap-y-12 justify-between">
+          <div className="w-full flex flex-col items-center justify-center lg:gap-y-12 gap-y-4">
+            <Typography variant="h3" fontWeight="bold" className="self-start lg:self-center lg:h2">
+              Sign in
+            </Typography>
+            <div className="w-full flex flex-col items-center lg:justify-center lg:gap-y-5 gap-y-4">
               <div className="w-full relative">
-                <div className="absolute h-full flex items-center justify-center ml-2">
-                  <UserCircle size={20} color="#7F8EA3" />
+                <div className="absolute h-full flex items-center justify-center ml-3">
+                  <UserCircle size={20} className="text-muted-foreground" />
                 </div>
                 <Input type="email" placeholder="Email or Phone number"></Input>
               </div>
               <div className="w-full">
                 <div className="w-full relative pb-1">
-                  <div className="absolute h-full flex items-center justify-center ml-2">
-                    <Key size={20} color="#7F8EA3" />
+                  <div className="absolute h-full flex items-center justify-center ml-3">
+                    <Key size={20} className="text-muted-foreground" />
                   </div>
                   <Input type="password" placeholder="Password"></Input>
                 </div>
-                <div className="text-h6 font-regular text-white text-start">Forget Password?</div>
+                <Link href="/" className="text-h6 font-regular text-white text-start">
+                  Forget Password?
+                </Link>
               </div>
             </div>
           </div>
-          <div className="w-full flex flex-col justify-center items-center text-white font-regular">
-            <Button className="bg-[#FF5E3A] text-h5">Sign In</Button>
-            <div className="text-h6">
-              Don't have an account? <span className="text-[#FF5E3A]">Sign up</span>
-            </div>
+          <div className="w-full flex flex-col justify-center items-center text-white font-regular gap-y-2">
+            <Button className="text-h5 w-[100%] lg:w-[108px]">Sign In</Button>
+            <Typography variant="h6" fontWeight="regular">
+              Don't have an account?
+              <Link href="/" className="text-primary pl-1">
+                Sign up
+              </Link>
+            </Typography>
           </div>
         </div>
       </form>
