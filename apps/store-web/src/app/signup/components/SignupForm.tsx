065a4ad1bdcc1fe8ad32c3a2e54ee03ef0802b00
noeinatha <noeieang12@gmail.com>
2024-01-30 22:36:18 +0700

    implement signup page ui

---
+++ apps/store-web/src/app/signup/components/SignupForm.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import signup from '@/assets/images/Saly-1.png'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import Typography from '@/components/ui/typography'
+import { Key, Phone, UserCircle } from '@phosphor-icons/react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function SignupForm() {
+  return (
+    <div className="h-fit w-fit flex items-center flex-col md:flex-row md:pl-10 my-auto">
+      <div className="h-[295px] w-[238px] md:hidden">
+        <Image src={signup} width={0} height={0} sizes="100vw" alt="Signup Picture" />
+      </div>
+
+      <div className="h-fit w-fit flex flex-col items-center gap-4 md:gap-12 my-auto">
+        <Typography variant="h3" fontWeight="bold" className="self-start md:self-center md:h2">
+          Sign up
+        </Typography>
+
+        <div className="w-full flex flex-col gap-y-5">
+          <div className="w-full relative">
+            <div className="absolute h-full flex items-center justify-center ml-3">
+              <UserCircle size={20} className="text-muted-foreground" />
+            </div>
+            <Input type="email" placeholder="Email" className="pl-9" />
+          </div>
+          <div className="w-full relative">
+            <div className="absolute h-full flex items-center justify-center ml-3">
+              <Phone size={20} className="text-muted-foreground" />
+            </div>
+            <Input type="tel" placeholder="Phone number" className="pl-9" />
+          </div>
+          <div className="w-full relative">
+            <div className="absolute h-full flex items-center justify-center ml-3">
+              <Key size={20} className="text-muted-foreground" />
+            </div>
+            <Input type="password" placeholder="Password" className="pl-9" />
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center gap-y-2">
+          <Button className="bg-primary w-[100%] md:w-[108px]">Sign up</Button>
+          <div className="text-h6 flex flex-row gap-x-1">
+            <Typography variant="h6"> Already have an account?</Typography>
+            <Link href="../signin" className="text-primary">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-[602px] w-[497px] hidden md:block">
+        <Image src={signup} width={0} height={0} sizes="100vw" alt="Signup Picture" />
+      </div>
+    </div>
+  )
+}
---
+++ apps/store-web/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from './components/SignupForm'
+
+export default function SignupPage() {
+  return (
+    <div className="h-screen w-screen bg-background justify-items-center my-auto grid">
+      <SignupForm />
+    </div>
+  )
+}
