307de66640ebaac73ce669cb8cdac28846fde166
KKhimmoon <khin292546@gmail.com>
2024-01-31 00:21:36 +0700

    edit style in loginFrom

--- apps/store-web/src/app/signin/page.tsx
+++ apps/store-web/src/app/signin/page.tsx
@@ -9,8 +9,8 @@ import SigninForm from './components/SigninForm'
 
 export default function SignInPage() {
   return (
-    <main className="flex w-full bg-[#030711] min-h-screen lg:flex-row md:flex-col items-center justify-between p-24">
-      <div className="flex lg:w-1/2 min-w-238 h-full justify-center">
+    <main className="flex w-full bg-background min-h-screen lg:flex-row flex-col items-center justify-between p-24">
+      <div className="flex lg:w-1/2 min-w-[238px] min-h-[295px] h-full justify-center">
         <Image src="/login.svg" alt="Sign in picture" width={497} height={602} />
       </div>
       <div className="flex min-w-216 lg:w-1/2 h-full pr-10 md:pr-0">
