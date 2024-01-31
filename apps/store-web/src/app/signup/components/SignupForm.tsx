55715555713ed96d8da98aaca07ad5dea7b9781a
noeinatha <noeieang12@gmail.com>
2024-01-31 15:38:20 +0700

    Show signup data in console + check validation.

--- apps/store-web/src/app/signup/components/SignupForm.tsx
+++ apps/store-web/src/app/signup/components/SignupForm.tsx
@@ -1,21 +1,56 @@
 'use client'
 
-import signup from '@/assets/images/Saly-1.png'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import signupLogo from '@/assets/images/Saly-1.png'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Typography from '@/components/ui/typography'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Key, Phone, UserCircle } from '@phosphor-icons/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { z } from 'zod'
+
+import { useMutation } from '../../../../gqty'
+
+type Inputs = {
+  email: string
+  password: string
+}
+
+const validationSchema = z.object({
+  email: z.string().min(1, { message: 'Email is required' }).email({
+    message: 'Must be a valid email',
+  }),
+  password: z.string().min(6, { message: 'Password must be atleast 6 characters' }),
+})
+
+type ValidationSchema = z.infer<typeof validationSchema>
 
 export default function SignupForm() {
+  const { register, handleSubmit } = useForm<ValidationSchema>({
+    resolver: zodResolver(validationSchema),
+  })
+
+  const [signup, { isLoading }] = useMutation((mutation, { data }) => {
+    return mutation.createUser({ data })
+  })
+
+  // const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  const onSubmit: SubmitHandler<ValidationSchema> = (data) => console.log(data)
+
   return (
     <div className="h-fit w-fit flex items-center flex-col md:flex-row md:pl-10 my-auto">
       <div className="h-[295px] w-[238px] md:hidden">
-        <Image src={signup} width={0} height={0} sizes="100vw" alt="Signup Picture" />
+        <Image src={signupLogo} width={0} height={0} sizes="100vw" alt="Signup Picture" />
       </div>
 
-      <div className="h-fit w-fit flex flex-col items-center gap-4 md:gap-12 my-auto">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="h-fit w-fit flex flex-col items-center gap-4 md:gap-12 my-auto"
+      >
         <Typography variant="h3" fontWeight="bold" className="self-start md:self-center md:h2">
           Sign up
         </Typography>
@@ -25,24 +60,36 @@ export default function SignupForm() {
             <div className="absolute h-full flex items-center justify-center ml-3">
               <UserCircle size={20} className="text-muted-foreground" />
             </div>
-            <Input type="email" placeholder="Email" className="pl-9" />
+            <Input type="email" placeholder="Email" className="pl-9" {...register('email')} />
           </div>
           <div className="w-full relative">
             <div className="absolute h-full flex items-center justify-center ml-3">
               <Phone size={20} className="text-muted-foreground" />
             </div>
-            <Input type="tel" placeholder="Phone number" className="pl-9" />
+            <Input
+              type="tel"
+              placeholder="Phone number"
+              className="pl-9"
+              // {...register('tel', { required: true })}
+            />
           </div>
           <div className="w-full relative">
             <div className="absolute h-full flex items-center justify-center ml-3">
               <Key size={20} className="text-muted-foreground" />
             </div>
-            <Input type="password" placeholder="Password" className="pl-9" />
+            <Input
+              type="password"
+              placeholder="Password"
+              className="pl-9"
+              {...register('password')}
+            />
           </div>
         </div>
 
         <div className="flex flex-col items-center gap-y-2">
-          <Button className="bg-primary w-[100%] md:w-[108px]">Sign up</Button>
+          <Button type="submit" disabled={isLoading} className="bg-primary w-[100%] md:w-[108px]">
+            Sign up
+          </Button>
           <div className="text-h6 flex flex-row gap-x-1">
             <Typography variant="h6"> Already have an account?</Typography>
             <Link href="../signin" className="text-primary">
@@ -50,10 +97,10 @@ export default function SignupForm() {
             </Link>
           </div>
         </div>
-      </div>
+      </form>
 
       <div className="h-[602px] w-[497px] hidden md:block">
-        <Image src={signup} width={0} height={0} sizes="100vw" alt="Signup Picture" />
+        <Image src={signupLogo} width={0} height={0} sizes="100vw" alt="Signup Picture" />
       </div>
     </div>
   )
