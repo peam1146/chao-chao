2eadb118b35ec88b621bf02079fc6476fcc1fd04
KKhimmoon <khin292546@gmail.com>
2024-02-02 22:14:46 +0700

    fix code in Form

--- apps/store-web/src/app/signin/actions/userLogin.ts
+++ apps/store-web/src/app/signin/actions/userLogin.ts
@@ -6,7 +6,6 @@ import { redirect } from 'next/navigation'
 import { resolve } from '../../../../gqty'
 
 export async function userLogin(data: FormData) {
-  console.log(data)
   try {
     const { token } = await resolve(({ mutation }) => {
       const loginInfo = mutation.loginUser({
@@ -18,7 +17,6 @@ export async function userLogin(data: FormData) {
 
     cookies().set('payload-token', token!, { secure: false })
   } catch (e) {
-    console.error('Login failed:', e)
     redirect('/signin')
   }
   redirect('/')
