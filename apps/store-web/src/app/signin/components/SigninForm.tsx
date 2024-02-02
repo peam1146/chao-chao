9097d376a03a3ae29522d86b743a91b506ee8b35
KKhimmoon <khin292546@gmail.com>
2024-02-02 21:59:18 +0700

    login feat login cookie

--- apps/store-web/src/app/signin/components/SigninForm.tsx
+++ apps/store-web/src/app/signin/components/SigninForm.tsx
@@ -1,40 +1,16 @@
 'use client'
 
 import React from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Typography from '@/components/ui/typography'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { Key, UserCircle } from '@phosphor-icons/react'
 import Link from 'next/link'
-import { z } from 'zod'
 
 import { userLogin } from '../actions/userLogin'
 
-// interface IFormInput {
-//   email: string
-//   password: string
-// }
-const validationSchema = z.object({
-  email: z.string().min(1, { message: 'Email is required' }).email({
-    message: 'Must be a valid email',
-  }),
-  password: z.string().min(6, { message: 'Password must be at least 6 characters' }),
-})
-type ValidationSchema = z.infer<typeof validationSchema>
 export default function SigninForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ValidationSchema>({
-    resolver: zodResolver(validationSchema),
-  })
-  const onSubmit: SubmitHandler<ValidationSchema> = (data) => console.log(data)
-  // const { register, handleSubmit } = useForm<IFormInput>()
-  // const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
   return (
     <div className="w-full h-[339px]">
       <form className="w-full h-full" action={userLogin}>
@@ -55,9 +31,6 @@ export default function SigninForm() {
                   className="pl-8 max-w-[449px] text-white"
                 ></Input>
               </div>
-              {errors.email && (
-                <p className="text-xs italic text-red-500 mt-0.5">{errors.email?.message}</p>
-              )}
               <div className="w-full">
                 <div className="w-full relative pb-1">
                   <div className="absolute h-full flex items-center justify-center ml-3">
@@ -70,9 +43,6 @@ export default function SigninForm() {
                     className="pl-8 max-w-[449px] text-white"
                   ></Input>
                 </div>
-                {errors.password && (
-                  <p className="text-xs italic text-red-500 mt-2">{errors.password?.message}</p>
-                )}
                 <Link href="/" className="text-h6 font-regular text-white text-start">
                   Forget Password?
                 </Link>
