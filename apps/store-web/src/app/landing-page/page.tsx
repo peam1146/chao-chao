86fb4095a8a4f2fae1f2d2e7c2ff0c7e52f6903a
Thita <thita@kkumail.com>
2024-02-03 22:12:50 +0700

    feat: add card component (undone)

--- apps/store-web/src/app/landing-page/page.tsx
+++ apps/store-web/src/app/landing-page/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import Card from './components/card'
 import CategoryBlock from './components/category-block'
+import SmallCard from './components/small-card'
 
 export default function LandingPage() {
   return (
-    <main className="flex w-full bg-background min-h-screen lg:flex-row flex-col items-center justify-between p-24">
+    <main className="flex w-full h-full bg-background flex-col items-center justify-between p-24 gap-22">
       <CategoryBlock />
+      <div className="flex flex-col w-full gap-20">
+        <Card />
+        <SmallCard />
+      </div>
     </main>
   )
 }
