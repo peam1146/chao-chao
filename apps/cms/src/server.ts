d919d7235fb92f4ab5ad3e280ce9e28d31774c20
inwpuun <112232858+inwpuun@users.noreply.github.com>
2024-02-10 00:49:53 +0700

    feat(api): nodemailer for sent email

--- apps/cms/src/server.ts
+++ apps/cms/src/server.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import payload from "payload";
 
+import nodemailer from "nodemailer";
 require("dotenv").config();
 const app = express();
 
@@ -11,11 +12,27 @@ app.get("/", (_, res) => {
 });
 
 async function start() {
+	const transport = nodemailer.createTransport({
+		service: process.env.SMTP_SERVICE,
+		host: process.env.SMTP_HOST,
+		port: Number(process.env.SMTP_PORT),
+		secure: true,
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASS,
+		},
+	});
+
 	await payload.init({
 		secret: process.env.PAYLOAD_SECRET,
 		express: app,
 		onInit: async () => {
 			payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+		},
+		email: {
+			fromName: "inwpuun",
+			fromAddress: "inwpuun1@gmail.com",
+			transport,
 		},
 	});
 
