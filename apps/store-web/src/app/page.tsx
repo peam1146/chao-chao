6065f8e83eff71502c373db7765ad931af9a4f6d
Thita <thita@kkumail.com>
2024-02-10 20:31:27 +0700

    refactor: move page to home

--- apps/store-web/src/app/page.tsx
+++ apps/store-web/src/app/page.tsx
@@ -1,21 +1,24 @@
-import logo from '@/assets/images/logo.svg'
-import Typography from '@/components/ui/typography'
-import Image from 'next/image'
-
-import { resolve } from '../../gqty'
+import Card from './landing-page/components/card'
+import CategoryBlock from './landing-page/components/category-block'
+import SmallCard from './landing-page/components/small-card'
 
 export default async function Home() {
-  const data = await resolve(({ query }) => {
-    return query.meUser?.user?.email
-  })
+  const mockData = [
+    { name: 'Samsung Galaxy S21 Ultra 5G', rating: 4.0, price: 100 },
+    { name: 'Jujutsu kaisen Vol.4', rating: 4.0, price: 500 },
+    { name: 'Iphone 13 Pro Max', rating: 3.0, price: 20 },
+    { name: 'Macbook Pro 2021', rating: 2.0, price: 10 },
+  ]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row w-full items-center space-x-4">
-        <Typography variant="h1" fontWeight="bold" className="flex flex-row gap-4">
-          Welcome to
-          <Image src={logo} width={100} height={100} alt="logo" />
-          {data && <span>{data}</span>}
-        </Typography>
+    <main className="container flex w-full bg-background min-h-[calc(100vh-64px)] flex-col items-center justify-between pb-10">
+      <CategoryBlock />
+      <div className="flex flex-col w-full gap-20 pt-10">
+        <Card />
+        <div className="grid grid-cols-2 2xl:grid-cols-4 lg:grid-cols-3 gap-2 lg:gap-3">
+          {mockData.map((item, index) => (
+            <SmallCard key={index} name={item.name} rating={item.rating} price={item.price} />
+          ))}
+        </div>
       </div>
     </main>
   )
