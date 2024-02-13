9fa1145f056568d1ca0ed7f9730a2c5d62c8d2c8
msp5382 <meen.sankaew@gmail.com>
2024-02-13 12:15:56 +0700

    feat: search page

--- apps/store-web/src/components/layout/Navbar.tsx
+++ apps/store-web/src/components/layout/Navbar.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import logo from '@/assets/images/logo.svg'
 import { Input } from '@/components/ui/input'
 import { cn } from '@/lib/utils'
 import { List, MagnifyingGlass } from '@phosphor-icons/react'
+import { set } from 'date-fns'
+import { SearchIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -14,6 +16,40 @@ import { useMediaQuery } from 'usehooks-ts'
 import Typography from '../ui/typography'
 import { MenuSheet } from './components/MenuSheet'
 import { ModeToggle } from './components/theme-toggle'
+
+const SearchSuggestion = () => {
+  const [search, setSearch] = useState('')
+  return (
+    <div className="relative w-2/5">
+      <div
+        className={cn(
+          'flex h-10 w-full items-center rounded-md border border-input bg-background p-2 text-sm ring-offset-background',
+          search && 'rounded-b-none border-b-0'
+        )}
+      >
+        <SearchIcon className="h-4" />
+        <input
+          onChange={(e) => {
+            setSearch(e.target.value)
+          }}
+          placeholder="Search"
+          value={search}
+          className="w-full bg-transparent"
+        />
+      </div>
+      {search && (
+        <div className="bg-background text-muted-foreground text-sm w-full absolute border rounded-md rounded-t-none">
+          {
+            <div className="flex p-2 items-center">
+              <SearchIcon className="h-4" />
+              <p className="w-full">{search}</p>
+            </div>
+          }
+        </div>
+      )}
+    </div>
+  )
+}
 
 const NavbarDesktop = () => {
   const [openMenu, setOpenMenu] = useState(false)
@@ -28,7 +64,7 @@ const NavbarDesktop = () => {
           <Link href="/" className="flex items-center">
             <Image src={logo} width={70} height={70} alt="logo" />
           </Link>
-          <Input placeholder="Search" className="w-2/5" />
+          <SearchSuggestion />
           <div className="flex items-center gap-4">
             <Link href="/signin" className="flex flex-row gap-0.5 items-center">
               {pathname === '/signin' && <MagnifyingGlass className="w-4 h-4" />}
@@ -42,7 +78,7 @@ const NavbarDesktop = () => {
                 Sign up
               </Typography>
             </Link>
-            <ModeToggle />
+            <ModeToggle />S S
           </div>
         </div>
       </header>
