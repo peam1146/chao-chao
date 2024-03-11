3e341895d698b32cd65d65e49cb824b7974d9dd4
Thita <thita@kkumail.com>
2024-03-11 15:01:06 +0700

    feat: enable add/delete image
    Co-authored-by: Yatawee Boonpeng <6430086521@student.chula.ac.th>

--- apps/store-web/src/app/asset-regist/components/regist-card.tsx
+++ apps/store-web/src/app/asset-regist/components/regist-card.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChangeEvent, useEffect, useRef } from 'react'
+import { ChangeEvent, useRef } from 'react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -9,7 +9,6 @@ import { TooltipProvider } from '@/components/plate-ui/tooltip'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import {
   Select,
   SelectContent,
@@ -22,10 +21,10 @@ import Typography from '@/components/ui/typography'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Plus } from '@phosphor-icons/react'
 import { ListPlus, X, XCircle } from '@phosphor-icons/react'
+import { Value } from '@radix-ui/react-select'
 import Image from 'next/image'
 import { z } from 'zod'
 
-import TagsInput from '../TagsInput'
 import { PlateEditor } from './description'
 import { Tag, TagInput } from './tags/tag-input'
 
@@ -52,12 +51,24 @@ export default function RegistCard() {
     }
   }
 
+  const ListforImage = [mockPic, mockPic, mockPic, mockPic]
+  const [listImg, setListImg] = React.useState(ListforImage)
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0]
       console.log('Selected file:', file)
+      // const newImageUrl = URL.createObjectURL(file)
+      const newList = listImg.concat(mockPic)
+      setListImg(newList)
     }
   }
+  const handleDeleteImage = (idx: any) => {
+    const newImages = [...listImg]
+    newImages.splice(idx, 1)
+    setListImg(newImages)
+    console.log(listImg)
+  }
+
   type AssetSchema = z.infer<typeof assetSchema>
   const form = useForm<AssetSchema>({
     resolver: zodResolver(assetSchema),
@@ -65,13 +76,20 @@ export default function RegistCard() {
   async function onSubmit(data: z.infer<typeof assetSchema>) {
     console.log(data)
   }
-  const ListforImage = [mockPic, mockPic, mockPic, mockPic, mockPic, mockPic, mockPic, mockPic]
   const Demotags: Tag[] = [
     { id: Math.random().toString(36), text: 'Sports' },
     { id: Math.random().toString(36), text: 'Travel' },
     { id: Math.random().toString(36), text: 'Programming' },
   ]
+  const autoCompleteOptions: Tag[] = [
+    ...Demotags,
+    { id: Math.random().toString(36), text: 'Food' },
+    { id: Math.random().toString(36), text: 'Movies' },
+    { id: Math.random().toString(36), text: 'Art' },
+    { id: Math.random().toString(36), text: 'Books' },
+  ]
   const [tags, setTags] = React.useState<Tag[]>(Demotags)
+  const [tagsDB, setTagsDB] = React.useState<Tag[]>(autoCompleteOptions)
   return (
     <div className="flex flex-col w-full gap-4">
       <div className="flex flex-row gap-1">
@@ -88,7 +106,7 @@ export default function RegistCard() {
           >
             <div className="flex w-full lg:flex-row flex-col gap-8">
               {/*Name + Rental Fee*/}
-              <div className="flex flex-col max-w-[428px] w-full">
+              <div className="flex flex-col w-full lg:w-1/2">
                 <Typography variant="h5">Name</Typography>
                 <FormField
                   control={form.control}
@@ -104,8 +122,8 @@ export default function RegistCard() {
                 />
               </div>
               {/*Rental Fee*/}
-              <div className="flex flex-row items-end gap-[10px]">
-                <div className="flex flex-col w-full max-w-[428px] min-w-[100px]">
+              <div className="flex flex-row items-end gap-[10px] w-full lg:w-1/2">
+                <div className="flex flex-col  max-w-[428px] w-full min-w-[100px]">
                   <Typography variant="h5">Rental Fee</Typography>
                   <FormField
                     control={form.control}
@@ -121,12 +139,12 @@ export default function RegistCard() {
                 </div>
                 <Select>
                   <SelectTrigger className="w-[106px]">
-                    <SelectValue placeholder="฿/day" />
+                    <SelectValue placeholder="฿ /day" />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectGroup>
-                      <SelectItem value="฿/day">฿/day</SelectItem>
-                      <SelectItem value="฿/month">฿/month</SelectItem>
+                      <SelectItem value="฿/day">฿ /day</SelectItem>
+                      <SelectItem value="฿/month">฿ /month</SelectItem>
                     </SelectGroup>
                   </SelectContent>
                 </Select>
@@ -146,14 +164,19 @@ export default function RegistCard() {
                       <div>
                         <TagInput
                           {...field}
-                          placeholder="Enter a topic"
+                          id="category"
+                          placeholder="Enter new category"
                           tags={tags}
-                          className="sm:min-w-[450px]"
+                          // enableAutocomplete={
+                          //   document.activeElement?.id === 'category' ? true : false
+                          // }
+                          enableAutocomplete={true}
+                          autocompleteOptions={tagsDB}
+                          className="w-full"
                           setTags={(newTags) => {
                             setTags(newTags)
                           }}
                         />
-                        {/* <TagsInput {...field} /> */}
                       </div>
                     </FormControl>
                   </FormItem>
@@ -171,7 +194,7 @@ export default function RegistCard() {
               {/*Image*/}
               <Typography variant="h5">Image</Typography>
 
-              <div className="grid gap-2 2xl:grid-cols-8 xl:grid-cols-7 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-3 grid-cols-3">
+              <div className="grid gap-2 2xl:grid-cols-8 xl:grid-cols-7 lg:grid-cols-5 md:grid-cols-3 grid-cols-3">
                 <div className="flex md:w-[120px] w-[80px] md:h-[120px] h-[80px] justify-center items-center rounded-[8px] border-primary border-dashed border-2">
                   <p onClick={handleButtonClick}>
                     <Plus className="w-4 h-4 text-primary" />
@@ -183,7 +206,7 @@ export default function RegistCard() {
                     onChange={handleFileChange}
                   />
                 </div>
-                {ListforImage.map((item, index) => (
+                {listImg.map((item, index) => (
                   <div className="relative">
                     {/* <div className="w-[24px] h-[24px] rounded-full bg-primary justify-center items-center">
                       <X size={16} />
@@ -191,8 +214,8 @@ export default function RegistCard() {
                     <XCircle
                       size={24}
                       weight="fill"
-                      className="absolute md:right-1 md:top-1 right-3 top-1 text-primary"
-                      // onClick={}
+                      className="absolute lg:right-1 lg:top-1 right-7 top-1 text-primary"
+                      onClick={handleDeleteImage.bind(index)}
                     />
                     <Image
                       key={index}
