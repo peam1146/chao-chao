92c5457fa42babca822c52375e6953e4bb8b7cf3
inwpuun <112232858+inwpuun@users.noreply.github.com>
2024-03-19 14:39:16 +0700

    feat(web): modal rental request

--- apps/store-web/src/app/rentalRequest/components/RequestReceivedContainer.tsx
+++ apps/store-web/src/app/rentalRequest/components/RequestReceivedContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	RentalRequestCard,
 	RentalRequestCardButton,
@@ -10,68 +10,129 @@ import { Check } from "@phosphor-icons/react/dist/ssr";
 import { X } from "@phosphor-icons/react/dist/csr/X";
 import Typography from "@/components/ui/typography";
 import { useMediaQuery } from "@/hooks/use-media-query";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import FileUpload from "@/components/ui/fileUpload";
 
 export default function RequestReceivedContainer() {
 	const isDesktop = useMediaQuery("(min-width: 821px)");
+	const [open, setOpen] = useState(true);
 
 	return (
-		<div className="flex flex-col gap-4 w-full">
-			<RentalRequestCard>
-				<RentalRequestCardContainer
-					title="จกรยานแพะภเขา"
-					requestFrom="มาย มม"
-					thumbnail={bicycle}
-					startDate={new Date("2024-02-29")}
-					endDate={new Date()}
-					rentalFee={50}
-				/>
-				<RentalRequestCardButton>
-					<Button
-						variant="default"
-						size={isDesktop ? "sm" : "lg"}
-						className="flex gap-2 w-full"
-					>
-						<Check className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
-						<Typography variant={isDesktop ? "h6" : "h5"}>Accept</Typography>
-					</Button>
-					<Button
-						variant="outline"
-						size={isDesktop ? "sm" : "lg"}
-						className="flex gap-2 w-full"
-					>
-						<X className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
-						<Typography variant={isDesktop ? "h6" : "h5"}>Decline</Typography>
-					</Button>
-				</RentalRequestCardButton>
-			</RentalRequestCard>
-			<RentalRequestCard>
-				<RentalRequestCardContainer
-					title="จกรยานแพะภเขา"
-					requestFrom="มาย มม"
-					thumbnail={bicycle}
-					startDate={new Date("2024-02-29")}
-					endDate={new Date()}
-					rentalFee={50}
-				/>
-				<RentalRequestCardButton>
-					<Button
-						variant="default"
-						size={isDesktop ? "sm" : "lg"}
-						className="flex gap-2 w-full"
-					>
-						<Check className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
-						<Typography variant={isDesktop ? "h6" : "h5"}>Accept</Typography>
-					</Button>
-					<Button
-						variant="outline"
-						size={isDesktop ? "sm" : "lg"}
-						className="flex gap-2 w-full"
-					>
-						<X className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
-						<Typography variant={isDesktop ? "h6" : "h5"}>Decline</Typography>
-					</Button>
-				</RentalRequestCardButton>
-			</RentalRequestCard>
-		</div>
+		<>
+			<div className="flex flex-col gap-4 w-full">
+				<RentalRequestCard>
+					<RentalRequestCardContainer
+						title="จกรยานแพะภเขา"
+						requestFrom="มาย มม"
+						thumbnail={bicycle}
+						startDate={new Date("2024-02-29")}
+						endDate={new Date()}
+						rentalFee={50}
+					/>
+					<RentalRequestCardButton>
+						<Button
+							variant="default"
+							size={isDesktop ? "sm" : "lg"}
+							className="flex gap-2 w-full"
+							onClick={() => setOpen(true)}
+						>
+							<Check className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
+							<Typography variant={isDesktop ? "h6" : "h5"}>Accept</Typography>
+						</Button>
+						<Button
+							variant="outline"
+							size={isDesktop ? "sm" : "lg"}
+							className="flex gap-2 w-full"
+						>
+							<X className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
+							<Typography variant={isDesktop ? "h6" : "h5"}>Decline</Typography>
+						</Button>
+					</RentalRequestCardButton>
+				</RentalRequestCard>
+				<RentalRequestCard>
+					<RentalRequestCardContainer
+						title="จกรยานแพะภเขา"
+						requestFrom="มาย มม"
+						thumbnail={bicycle}
+						startDate={new Date("2024-02-29")}
+						endDate={new Date()}
+						rentalFee={50}
+					/>
+					<RentalRequestCardButton>
+						<Button
+							variant="default"
+							size={isDesktop ? "sm" : "lg"}
+							className="flex gap-2 w-full"
+							onClick={() => setOpen(true)}
+						>
+							<Check className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
+							<Typography variant={isDesktop ? "h6" : "h5"}>Accept</Typography>
+						</Button>
+						<Button
+							variant="outline"
+							size={isDesktop ? "sm" : "lg"}
+							className="flex gap-2 w-full"
+						>
+							<X className="max-lg:w-5 max-lg:h-5 lg:w-4 lg:h-4" />
+							<Typography variant={isDesktop ? "h6" : "h5"}>Decline</Typography>
+						</Button>
+					</RentalRequestCardButton>
+				</RentalRequestCard>
+			</div>
+			<Dialog open={open} onOpenChange={setOpen}>
+				<DialogContent className="overflow-hidden w-[462px] p-0 shadow-lg">
+					<form className="flex flex-col p-6 gap-4">
+						<div className="flex flex-col gap-3">
+							<Typography variant="h4" fontWeight="bold">
+								Please fill in the required information.
+							</Typography>
+							<div className="flex max-lg:flex-col gap-4 w-full">
+								<div className="flex flex-col gap-1">
+									<Typography variant="h5">Insurance fee</Typography>
+									<Input
+										placeholder="฿0.00"
+										className="max-lg:w-full w-[200px] bg-card"
+									/>
+								</div>
+								<div className="flex flex-col gap-1">
+									<Typography variant="h5">Delivery fee</Typography>
+									<Input
+										placeholder="฿0.00"
+										className="max-lg:w-full w-[200px] bg-card"
+									/>
+								</div>
+							</div>
+							<div className="flex flex-col gap-1">
+								<Typography variant="h5">Contract file</Typography>
+								<FileUpload
+									uploadText="Drag a file here or "
+									uploadStatus="default"
+								/>
+							</div>
+						</div>
+						<div className="flex justify-end gap-4">
+							<Button
+								variant="outline"
+								type="button"
+								size="lg"
+								className="w-full lg:w-[108px]"
+								onClick={() => setOpen(false)}
+							>
+								<Typography variant="h5">Cancel</Typography>
+							</Button>
+							<Button
+								variant="default"
+								type="submit"
+								size="lg"
+								className="w-full lg:w-[108px]"
+							>
+								<Typography variant="h5">Submit</Typography>
+							</Button>
+						</div>
+					</form>
+				</DialogContent>
+			</Dialog>
+		</>
 	);
 }
