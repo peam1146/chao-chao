76de2ff8cf3da7565d24ee49d381f653fdc74590
noeinatha <noeieang12@gmail.com>
2024-03-27 01:19:06 +0700

    Add FAQ

--- apps/store-web/src/app/help/page.tsx
+++ apps/store-web/src/app/help/page.tsx
@@ -47,26 +47,54 @@ export default function HelpPage() {
         <div className="self-center lg:w-5/6 w-full">
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+              <AccordionTrigger>
+                What types of assets can I rent out on the platform?
+              </AccordionTrigger>
+              <AccordionContent>
+                Our platform accommodates a wide range of assets, including but not limited to
+                equipment, vehicles, spaces, tools, and even services. If you're unsure whether your
+                asset fits within our guidelines, feel free to reach out to our support team for
+                clarification.
+              </AccordionContent>
             </AccordionItem>
           </Accordion>
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+              <AccordionTrigger>
+                How does the chat feature work for communicating with renters/owners?
+              </AccordionTrigger>
+              <AccordionContent>
+                Our built-in chat feature allows seamless communication between renters and asset
+                owners. Once a rental inquiry is made or an asset is listed, both parties can engage
+                in real-time messaging within the platform to discuss rental terms, ask questions,
+                and finalize agreements.
+              </AccordionContent>
             </AccordionItem>
           </Accordion>
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+              <AccordionTrigger>
+                How can I purchase ads to promote my assets on the platform?
+              </AccordionTrigger>
+              <AccordionContent>
+                Users have the option to boost the visibility of their listed assets by purchasing
+                ad placements within our platform. Simply navigate to the advertising section in
+                your dashboard, choose your desired ad format (e.g., banner, sponsored listing), set
+                your budget, and target audience, and launch your ad campaign.
+              </AccordionContent>
             </AccordionItem>
           </Accordion>
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+              <AccordionTrigger>
+                Is there a rating system for renters and asset owners?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes, we encourage users to provide feedback and ratings based on their rental
+                experiences to foster transparency and trust within our community. Both renters and
+                asset owners can rate each other based on factors such as communication,
+                reliability, and overall satisfaction.
+              </AccordionContent>
             </AccordionItem>
           </Accordion>
         </div>
