35e728ba43a860258710da50ddd24e31969e0092
OakNuttawash <111405686+OakNuttawash@users.noreply.github.com>
2024-04-15 14:32:02 +0700

    refactor: payment

--- apps/store-web/src/app/(manageAssets)/payment/components/HistoryContainer.tsx
+++ apps/store-web/src/app/(manageAssets)/payment/components/HistoryContainer.tsx
@@ -7,8 +7,10 @@ import { Modal } from '@/components/ui/dialog'
 import { Spinner } from '@/components/ui/spinner'
 import Typography from '@/components/ui/typography'
 import { useUserToken } from '@/providers/User'
+import { Separator } from '@radix-ui/react-separator'
+import Image from 'next/image'
 
-import { Renting_status_Input, useQuery } from '../../../../../gqty'
+import { Payment_type_Input, useQuery } from '../../../../../gqty'
 import { PaymentCard, PaymentCardContainer } from './PaymentCard'
 import PaymentHistoryLessorReviewModal from './components/PaymentHistoryLessorReviewModal'
 import PaymentHistoryRenterReviewModal from './components/PaymentHistoryRenterReviewModal'
@@ -21,29 +23,49 @@ export default function HistoryContainer() {
 
   const { userId } = useUserToken()
 
-  const pay = query
-    .Rentings({
+  const payRenting = query
+    .Payments({
       where: {
-        status: { equals: Renting_status_Input.COMPLETED },
+        type: {
+          equals: Payment_type_Input.RENTING,
+        },
       },
     })
-    ?.docs?.filter((renting) => renting?.rentedBy?.user?.id === Number(userId))
-    ?.map((renting) => ({
+    ?.docs?.filter((renting) => renting?.createdBy.id === Number(userId))
+    .map((renting) => ({
       ...renting,
     }))
     .filter((renting) => renting?.id !== undefined)
 
-  const receive = query
-    .Rentings({
+  const receiveRenting = query
+    .Payments({
       where: {
-        status: { equals: Renting_status_Input.COMPLETED },
+        type: {
+          equals: Payment_type_Input.RENTING,
+        },
       },
     })
-    ?.docs?.filter((renting) => renting?.rentedTo?.user?.id === Number(userId))
-    ?.map((renting) => ({
+    ?.docs?.filter((renting) => renting?.payTo?.id === Number(userId))
+    .map((renting) => ({
       ...renting,
     }))
     .filter((renting) => renting?.id !== undefined)
+
+  const payAdvertise = query
+    .Payments({
+      where: {
+        type: {
+          equals: Payment_type_Input.ADVERTISE,
+        },
+      },
+    })
+    ?.docs?.filter((renting) => renting?.createdBy.id === Number(userId))
+    .map((renting) => ({
+      ...renting,
+    }))
+    .filter((renting) => renting?.id !== undefined)
+
+  console.log(payAdvertise?.at(0)?.advertise?.price)
 
   const [openLessorReviewModal, SetOpenLessorReviewModal] = useState(false)
   const [openRenterReveiwModal, setOpenRenterReviewModal] = useState(false)
@@ -51,7 +73,7 @@ export default function HistoryContainer() {
   const [reviewToRentorId, setReviewToRentorId] = useState<number | undefined>(undefined)
   const [reviewToItemId, setReviewToItemId] = useState<number | undefined>(undefined)
 
-  if (query.$state.isLoading || query.$state.error) {
+  if (query.$state.isLoading) {
     return (
       <div className="flex justify-center">
         <Spinner className="self-center" />
@@ -59,29 +81,31 @@ export default function HistoryContainer() {
     )
   }
 
-  if (pay?.length === 0 && receive?.length === 0) {
+  if (payRenting?.length === 0 && receiveRenting?.length === 0 && payAdvertise?.length === 0) {
     return <div className="flex justify-center">No payment history</div>
   }
 
   return (
     <div className="flex flex-col gap-4 w-full">
-      {pay?.map((renting) => (
+      {payRenting?.map((renting) => (
         <PaymentCard>
           <PaymentCardContainer
-            title={renting.rentedTo?.item?.name ?? ''}
-            assetFrom={renting.rentedTo?.user?.firstName ?? ''}
+            title={renting?.renting?.renting?.rentedTo?.item?.name ?? ''}
+            assetFrom={renting?.renting?.renting?.rentedTo?.user?.firstName ?? ''}
             thumbnail={
-              renting.rentedTo?.item?.image?.length
-                ? renting.rentedTo?.item?.image[0].url ?? ''
+              renting?.renting?.renting?.rentedTo?.item?.image?.length
+                ? renting?.renting?.renting?.rentedTo?.item?.image[0].url ?? ''
                 : ''
             }
-            startDate={new Date(renting.startDate ?? new Date())}
-            endDate={new Date(renting.endDate ?? new Date())}
-            insuranceFee={Number(renting.insuranceFee)}
-            deliveryFee={Number(renting.deliveryFee)}
-            rentalFee={Number(renting.rentalFee)}
+            startDate={new Date(renting?.renting?.renting?.startDate ?? new Date())}
+            endDate={new Date(renting?.renting?.renting?.endDate ?? new Date())}
+            insuranceFee={Number(renting?.renting?.renting?.insuranceFee)}
+            deliveryFee={Number(renting?.renting?.renting?.deliveryFee)}
+            rentalFee={Number(renting?.renting?.renting?.rentalFee)}
             totalFee={
-              Number(renting.insuranceFee) + Number(renting.deliveryFee) + Number(renting.rentalFee)
+              Number(renting?.renting?.renting?.insuranceFee) +
+              Number(renting?.renting?.renting?.deliveryFee) +
+              Number(renting?.renting?.renting?.rentalFee)
             }
             variant="expenses"
           />
@@ -89,16 +113,16 @@ export default function HistoryContainer() {
             <div className="flex xl:flex-col max-xl:flex-row-reverse max-xl:w-full gap-2 xl:w-max items-center justify-center">
               <Typography variant="h6" className="text-unavailable w-full text-center">
                 - ฿
-                {Number(renting.insuranceFee) +
-                  Number(renting.deliveryFee) +
-                  Number(renting.rentalFee)}
+                {Number(renting?.renting?.renting?.insuranceFee) +
+                  Number(renting?.renting?.renting?.deliveryFee) +
+                  Number(renting?.renting?.renting?.rentalFee)}
               </Typography>
               <Button
                 variant="default"
                 className="flex gap-2 w-full"
                 onClick={() => {
-                  setReviewToLessorId(Number(renting.rentedTo?.user?.id))
-                  setReviewToItemId(Number(renting.rentedTo?.item?.id))
+                  setReviewToLessorId(Number(renting?.renting?.renting?.rentedTo?.user?.id))
+                  setReviewToItemId(Number(renting?.renting?.renting?.rentedTo?.item?.id))
                   setOpenRenterReviewModal(true)
                 }}
               >
@@ -108,23 +132,25 @@ export default function HistoryContainer() {
           </div>
         </PaymentCard>
       ))}
-      {receive?.map((renting) => (
+      {receiveRenting?.map((renting) => (
         <PaymentCard>
           <PaymentCardContainer
-            title={renting.rentedTo?.item?.name ?? ''}
-            assetFrom={renting.rentedBy?.user?.firstName ?? ''}
+            title={renting?.renting?.renting?.rentedTo?.item?.name ?? ''}
+            assetFrom={renting?.renting?.renting?.rentedBy?.user?.firstName ?? ''}
             thumbnail={
-              renting.rentedTo?.item?.image?.length
-                ? renting.rentedTo?.item?.image[0].url ?? ''
+              renting?.renting?.renting?.rentedTo?.item?.image?.length
+                ? renting?.renting?.renting?.rentedTo?.item?.image[0].url ?? ''
                 : ''
             }
-            startDate={new Date(renting.startDate ?? new Date())}
-            endDate={new Date(renting.endDate ?? new Date())}
-            insuranceFee={Number(renting.insuranceFee)}
-            deliveryFee={Number(renting.deliveryFee)}
-            rentalFee={Number(renting.rentalFee)}
+            startDate={new Date(renting?.renting?.renting?.startDate ?? new Date())}
+            endDate={new Date(renting?.renting?.renting?.endDate ?? new Date())}
+            insuranceFee={Number(renting?.renting?.renting?.insuranceFee)}
+            deliveryFee={Number(renting?.renting?.renting?.deliveryFee)}
+            rentalFee={Number(renting?.renting?.renting?.rentalFee)}
             totalFee={
-              Number(renting.insuranceFee) + Number(renting.deliveryFee) + Number(renting.rentalFee)
+              Number(renting?.renting?.renting?.insuranceFee) +
+              Number(renting?.renting?.renting?.deliveryFee) +
+              Number(renting?.renting?.renting?.rentalFee)
             }
             variant="receivable"
           />
@@ -132,20 +158,87 @@ export default function HistoryContainer() {
             <div className="flex xl:flex-col max-xl:flex-row-reverse max-xl:w-full gap-2 xl:w-max items-center justify-center">
               <Typography variant="h6" className="text-available w-full text-center">
                 + ฿
-                {Number(renting.insuranceFee) +
-                  Number(renting.deliveryFee) +
-                  Number(renting.rentalFee)}
+                {Number(renting?.renting?.renting?.insuranceFee) +
+                  Number(renting?.renting?.renting?.deliveryFee) +
+                  Number(renting?.renting?.renting?.rentalFee)}
               </Typography>
               <Button
                 variant="default"
                 className="flex gap-2 w-full"
                 onClick={() => {
-                  setReviewToRentorId(Number(renting.rentedBy?.user?.id))
+                  setReviewToRentorId(Number(renting?.renting?.renting?.rentedBy?.user?.id))
                   SetOpenLessorReviewModal(true)
                 }}
               >
                 <Typography>Write a review</Typography>
               </Button>
+            </div>
+          </div>
+        </PaymentCard>
+      ))}
+      {payAdvertise?.map((pay) => (
+        <PaymentCard>
+          <div className="flex gap-3 w-full pt-2 pb-2 ">
+            <Image
+              src={pay?.advertise?.item?.image?.at(0)?.url ?? ''}
+              alt="thumbnail"
+              className="w-[100px] h-[100px]"
+              width={100}
+              height={100}
+            />
+            <div className="flex flex-col gap-1 w-full">
+              <Button variant="link" className="p-0 w-fit h-fit">
+                <Typography variant="h5">{pay?.advertise?.item?.name}</Typography>
+              </Button>
+              <Separator
+                orientation="horizontal"
+                className="border-b border-border whitespace-nowrap"
+              />
+              <div className="flex flex-col gap-1">
+                <div className="flex xl:flex-row flex-col xl:gap-8 gap-1">
+                  <div className="flex w-[180px] gap-1">
+                    <Typography variant="h6" className="text-light">
+                      asset from:
+                    </Typography>
+                    <Button variant="link" className="p-0 w-fit h-fit">
+                      <Typography variant="h6">{pay?.createdBy?.firstName}</Typography>
+                    </Button>
+                  </div>
+                  <div className="flex max-xl:flex-col xl:flex-row max-xl:gap-1 xl:gap-8">
+                    <div className="flex gap-1">
+                      <Typography variant="h6" className="text-light">
+                        Date:
+                      </Typography>
+                      <Typography variant="h6">{`${new Date(
+                        pay?.advertise?.startDate ?? new Date()
+                      ).getDate()}/${new Date(
+                        pay?.advertise?.startDate ?? new Date()
+                      ).toLocaleString('default', {
+                        month: '2-digit',
+                        year: '2-digit',
+                      })} - ${new Date(pay?.advertise?.endDate ?? new Date()).getDate()}/${new Date(
+                        pay?.advertise?.endDate ?? new Date()
+                      ).toLocaleString('default', {
+                        month: '2-digit',
+                        year: '2-digit',
+                      })}`}</Typography>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex gap-2">
+                  <Typography variant="h6" className="text-light">
+                    Advertise fee:
+                  </Typography>
+                  <Typography variant="h6">{`฿${pay?.advertise?.price}`}</Typography>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center items-center xl:w-max max-xl:w-full px-5">
+            <div className="flex xl:flex-col max-xl:flex-row-reverse max-xl:w-full gap-2 xl:w-max items-center justify-center">
+              <Typography variant="h6" className="text-unavailable w-full text-center">
+                - ฿{pay?.advertise?.price}
+              </Typography>
             </div>
           </div>
         </PaymentCard>
