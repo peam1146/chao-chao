43be7ac9289caf566d85ddceeb14fee244c1af31
Thita <thita@kkumail.com>
2024-04-19 22:06:06 +0700

    fix: reduce wait time

--- apps/store-web/web-e2e/cypress/e2e/register.cy.ts
+++ apps/store-web/web-e2e/cypress/e2e/register.cy.ts
@@ -1,9 +1,9 @@
 describe('register', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/', { failOnStatusCode: false })
-    cy.wait(3000)
+    cy.wait(1000)
     cy.get('p').contains('Sign up').click()
-    cy.wait(3000)
+    cy.wait(1000)
   })
 
   it('TC1-1', () => {
